8f1e2dd3933a2a68229133726de5470176760a71
yeskend1r <eskendir132@gmail.com>
2023-03-14 20:50:36 +0600

    Qotaq zhe

--- lib/loginpage.dart
+++ lib/loginpage.dart
@@ -36,6 +36,7 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        resizeToAvoidBottomInset: false,
         backgroundColor: Colors.grey[300],
         body: SafeArea(
           child: Center(
